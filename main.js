46d7ad28f90e7a6b41b89fcaef391720ae6d9b4a
unknown <olha1boiko@ukr.net>
2018-09-23 19:49:11 +0300

    Add functional for select-list

--- main.js
+++ main.js
@@ -3,26 +3,54 @@ const modalOverlay = document.querySelector("#modal-overlay");
 const closeButton = document.querySelector("#close-button");
 const openModal = document.getElementsByClassName('source');
 const authorList = document.querySelector('#authorList');
-//------create form-elements
-const authorNameLabel = document.createElement('div');
- authorNameLabel.innerHTML = '<lable for="main-name">Прізвище автора</lable> <input name="main-name" id="main-name"/><lable>  Його ініціали</lable> <input /> </br></br>'
-function runSelect() {
-const numberOfAuthors = document.querySelector('#number-of-authors').value;
-return numberOfAuthors;
+//------create form-elements----------------
+
+// Add new block with class tmp
+function addBlock(html) {
+  const authorNameLabel = document.createElement('div');
+  authorNameLabel.className = "tmp";
+  authorNameLabel.innerHTML = (html) ? html : '<lable for="main-name">Прізвище автора:</lable> <input class="main-name" /><lable for="last-author-name"> Його ініціали:</lable><input name="last-author-name" class="name" /></br></br>';
+  return authorNameLabel;
 }
-/*  switch ()
-    case :*/
+// Clean blocks with class tmp
+function removeBlocks() {
+  const blocks = document.querySelectorAll('.tmp');
+  blocks.forEach(function(el){
+    el.remove();
+  });
+}
+
+function runSelect() {
+ removeBlocks();
+ const numberOfAuthors = document.getElementById('number-of-authors').value;
+ switch (numberOfAuthors) {
+     case '1': authorList.appendChild(addBlock());
+
+       break;
+     case '2': authorList.appendChild(addBlock());
+               authorList.appendChild(addBlock());
+
+       break;
+     case '7': for (let i = 0; i < 7; i++) {
+               authorList.appendChild(addBlock());
+             }
+       break;
+     case 'more-then-8': for (let i = 0; i < 6; i++) {
+                          authorList.appendChild(addBlock());
+                         }
+                         authorList.appendChild(addBlock('<lable for="last-author-main-name">Прізвище ОСТАННЬОГО автора</lable> <input name="last-author-main-name"/><lable>  Його ініціали</lable for="last-author-name"> <input name="last-author-name" class="name" /> </br></br>'));
+                         break;
+     case 'organisation': authorList.appendChild(addBlock('<lable for="organization-name">Назва установи, що підготувала дану працю: <input name="organization-name"/> </br></br>'));
+       break;
+   }
+}
 
 
 openModal.item(0).addEventListener('click', function (){
   showModalWindow();
   authorList.style.display = 'block';
-    switch (strNumber) {
-      
 
-    }
 
-  authorList.appendChild(authorNameLabel);
 
 });
 
